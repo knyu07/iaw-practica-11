0a2cd03203bb6dcc7dd9e8518cbec67db3df1e9e
knyu07 <pventura9839@gmail.com>
2021-01-20 12:21:42 +0100

    Terminado

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# iaw-practica-11
+# Práctica 11: Auditría de seguridad en Wordpress
+
+La herramienta WPScan CLI es un escáner de seguridad de WordPress de caja negra gratuito, para uso no comercial, escrito para profesionales de la seguridad y mantenedores de blogs para probar la seguridad de sus sitios.
+
+## Instalación:
+
+Instalamos Ruby
+```
+apt install ruby -y
+```
+
+Instalamos las dependencias
+```
+apt install build-essential libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev  libgmp-dev zlib1g-dev -y
+```
+
+Instalamos el WPscan
+```
+gem install wpscan 
+```
+
+Una vez instalado el WPScan con el comando ***wpscan --help* comprobamos.
+
+![](Imágenes/wcscan.png) 
+
+Para obtener la lista de plugins instalados de nuestro sitio ejecutamos:
+```
+wpscan --url DIRECCIÓN --enumerate p
+```
---
+++ wpscan.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+set -x
+
+#VARIABLES
+BD_ROOT_PASSWD=root
+DB_NAME=wordpress_db
+DB_USER=wordpress_user
+DB_PASSWORD=wordpress_password
+
+# Actualizamos la lista de paquetes
+apt update
+
+#Actualizamos los paquete
+apt upgrade -y
+
+# Instalamos el servidor web Apache
+apt install apache2 -y
+
+# Instalamos el MySQL Server
+apt install mysql-server -y
+
+# Instalamos los módulos de PHP
+apt install php libapache2-mod-php php-mysql -y
+
+#Descargamos el archivo wp-cli.phar
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+
+#Asignamos permisos de ejecución
+chmod +x wp-cli.phar
+
+#Movemos el archivo al directorio /usr/local/bin
+mv wp-cli.phar /usr/local/bin/wp
+
+------------------ INSTALACIÓN WORDPRESS CON WP-CLI ---------------------
+
+#Nos situamos en el directorio donde vamos a realizar la instalación.
+cd /var/www/html
+
+#Descargamos el código fuente de Wordpress
+wp core download --locale=es_ES --allow-root
+
+#Creamos la base de datos
+mysql -u root <<< "DROP DATABASE IF EXISTS $DB_NAME;"
+mysql -u root <<< "CREATE DATABASE $DB_NAME;"
+mysql -u root <<< "CREATE USER $DB_USER@'localhost' IDENTIFIED BY '$DB_PASSWORD';"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'localhost';"
+mysql -u root <<< "FLUSH PRIVILEGES;"
+
+#Creamos el archivo de configuración
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --allow-root
+
+#Instalamos Wordpress
+wp core install --url=http://3.80.141.180 --title="IAW" --admin_user=admin --admin_password=admin --admin_email=admin@admin.com --allow-root
+
+#Eliminamos el index.html 
+cd /var/www/html
+rm -rf index.html
+
+#Reiniciamos apache
+systemctl restart apache2.service
+
+----------------- PERSONALIZAR WORDPRESS ---------------------
+# Actualizamos los plugins
+wp plugin update  --path=/var/www/html/ --all --allow-root
+
+#Actualizamos los temas
+wp theme update --path=/var/www/html/ --all --allow-root
+
+#Instalar y activar plugin
+wp plugin install --path=/var/www/html/ contact-form-7 --activate --allow-root
+wp plugin install --path=/var/www/html/ jetpack --activate --allow-root
+
+#Activar tema
+wp theme install --path=/var/www/html/ oceanwp --activate --allow-root
+wp theme activate oceanwp
+
+--------------------- WPSCAN ----------------------------------
+
+#Instalamos Ruby
+apt install ruby -y
+
+#Instalamos las dependencias
+apt install build-essential libcurl4-openssl-dev libxml2 libxml2-dev libxslt1-dev ruby-dev  libgmp-dev zlib1g-dev -y
+
+#Instalamos el WPscan
+gem install wpscan 
